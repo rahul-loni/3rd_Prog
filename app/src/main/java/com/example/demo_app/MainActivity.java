52d2bd2585b9282fac78adfe79b544faf11be510
rahul Kumar <rahuliismt491@gmail.com>
2021-08-11 14:37:05 +0545

    Second Class

--- app/src/main/java/com/example/demo_app/MainActivity.java
+++ app/src/main/java/com/example/demo_app/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.demo_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+   Button btn_Signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btn_Signup=(Button)findViewById(R.id.btnSignup);
+        btn_Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_signup=new Intent(MainActivity.this,Signup.class);
+                startActivity(intent_signup);
+            }
+        });
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+
 }
---
+++ app/src/main/java/com/example/demo_app/Signup.java
@@ -0,0 +1,22 @@
+package com.example.demo_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+}
